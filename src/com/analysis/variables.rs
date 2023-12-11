9c6c9237ca5dea7f69d7985f81d7a93e3f03f457
catapillie <espritox@gmail.com>
2023-12-11 11:51:22 +0100

    look for variables in previous scopes

--- src/com/analysis/variables.rs
+++ src/com/analysis/variables.rs
@@ -8,11 +8,26 @@ use crate::com::{
 use super::Analyser;
 
 impl<'d> Analyser<'d> {
+    pub fn get_variable(&self, name: &str) -> Option<&(TypeAbt, u64)> {
+        let depth = self.current_depth;
+        let indices = self.current_offsets.iter().rev().enumerate();
+        for (i, &offset) in indices {
+            let depth = depth - i as u64 + 1;
+            let entry = (name.to_string(), depth, offset);
+
+            let var = self.variables.get(&entry);
+            if var.is_some() {
+                return var;
+            }
+        }
+        None
+    }
+
     pub fn declare_variable(&mut self, name: &str, ty: TypeAbt) -> u64 {
         let id = self.make_unique_id();
         let depth = self.current_depth;
         let offset = self.get_block_offset();
-        
+
         let entry = (name.to_string(), depth, offset);
         let previous = self.variables.insert(entry, (ty, id));
         assert!(previous.is_none());
@@ -36,15 +51,12 @@ impl<'d> Analyser<'d> {
         StmtAbtKind::Expr(Box::new(ExprAbt::Assignment(id, Box::new(bound_expr))))
     }
 
-    pub fn analyse_variable_expression(&mut self, id: &str, span: Span) -> ExprAbt {
-        let depth = self.current_depth;
-        let offset = self.get_block_offset();
-        let entry = (id.to_string(), depth, offset);
-        match self.variables.get(&entry) {
+    pub fn analyse_variable_expression(&mut self, name: &str, span: Span) -> ExprAbt {
+        match self.get_variable(name) {
             Some(&(_, id)) => ExprAbt::Variable(id),
             None => {
                 let d = diagnostics::create_diagnostic()
-                    .with_kind(DiagnosticKind::UnknownVariable(id.to_string()))
+                    .with_kind(DiagnosticKind::UnknownVariable(name.to_string()))
                     .with_severity(Severity::Error)
                     .with_span(span)
                     .done();
