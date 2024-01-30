dc173c2a8536c2fc0a77aa8676f6940c8f3c5812
catapillie <espritox@gmail.com>
2024-01-30 10:13:53 +0100

    add more array tests

--- src/tests/runtime.rs
+++ src/tests/runtime.rs
@@ -86,6 +86,8 @@ check_program_output!(tuple_indexing_nested => (bool, TypeAbt::Bool) true);
 
 check_program_output!(array_init => (bool, TypeAbt::Bool) true);
 check_program_output!(array_copy => (bool, TypeAbt::Bool) true);
+check_program_output!(array_as_func_arg => (bool, TypeAbt::Bool) true);
+check_program_output!(array_as_func_return => (bool, TypeAbt::Bool) true);
 
 macro_rules! check_program_output {
     ($name:ident => ($expected_ty:ty, $type_abt:expr) $expected:expr) => {
