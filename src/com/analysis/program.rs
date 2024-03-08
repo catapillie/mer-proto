e3a27f4c9075b68f0fb00e1fb00f7f5911dadec7
catapillie <espritox@gmail.com>
2024-03-08 14:54:39 +0100

    remove top level declaration scanning

--- src/com/analysis/program.rs
+++ src/com/analysis/program.rs
@@ -1,13 +1,12 @@
+use super::Analyser;
 use crate::{
     com::{
         abt::{self, FunctionInfo},
         ast,
     },
     diagnostics::{self, DiagnosticKind, Severity},
-    utils::{OptSpanned, Spanned},
+    utils::OptSpanned,
 };
-
-use super::Analyser;
 
 impl<'d> Analyser<'d> {
     pub fn analyse_program(mut self, ast: &ast::Stmt, expected_type: abt::Type) -> abt::Program {
@@ -31,10 +30,6 @@ impl<'d> Analyser<'d> {
                 code: None,
             },
         );
-
-        self.scope.depth = 1;
-        self.reach_top_level_declarations(ast);
-        self.scope.depth = 0;
 
         let abt = self.analyse_statement(ast);
         if !self.analyse_control_flow(&abt) {
@@ -65,47 +60,5 @@ impl<'d> Analyser<'d> {
         assert!(self.scope.is_root());
 
         self.program
-    }
-
-    fn reach_top_level_declarations(&mut self, ast: &ast::Stmt) {
-        let ast::StmtKind::Block(stmts) = &ast.value else {
-            return;
-        };
-
-        for stmt in stmts.iter() {
-            match &stmt.value {
-                ast::StmtKind::Func(Some(name), args, _, ty) => {
-                    let bound_args = args
-                        .iter()
-                        .map(|(name, ty, _)| (name.clone(), self.analyse_type(ty)))
-                        .collect();
-                    let bound_ty = self.analyse_type(ty);
-                    self.declare_function_here(
-                        name.clone().into(),
-                        bound_args,
-                        OptSpanned {
-                            value: bound_ty,
-                            span: Some(ty.span),
-                        },
-                    );
-                }
-                ast::StmtKind::DataDef(name, fields) => {
-                    let bound_fields = fields
-                        .iter()
-                        .map(|(name, ty)| {
-                            (
-                                name.clone(),
-                                Spanned {
-                                    value: self.analyse_type(ty),
-                                    span: ty.span,
-                                },
-                            )
-                        })
-                        .collect::<Vec<_>>();
-                    self.declare_data_structure_here(name, bound_fields);
-                }
-                _ => continue,
-            }
-        }
     }
 }
