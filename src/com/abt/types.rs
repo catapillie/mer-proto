e51fa5f9cf5b5a80408e64e15b60ce4b60f781fa
catapillie <espritox@gmail.com>
2024-03-22 14:28:41 +0100

    update 'is_known' function on types

--- src/com/abt/types.rs
+++ src/com/abt/types.rs
@@ -31,9 +31,26 @@ impl Type {
     pub fn is_known(&self) -> bool {
         match self {
             Type::Unknown => false,
-            Type::Ref(inner) => inner.is_known(),
-            Type::Func(args, ty) => args.iter().all(Self::is_known) && ty.is_known(),
-            _ => true,
+            Type::Never => true,
+            Type::Unit => true,
+            Type::U8 => true,
+            Type::U16 => true,
+            Type::U32 => true,
+            Type::U64 => true,
+            Type::I8 => true,
+            Type::I16 => true,
+            Type::I32 => true,
+            Type::I64 => true,
+            Type::F32 => true,
+            Type::F64 => true,
+            Type::Bool => true,
+            Type::Data(_) => true,
+            Type::Alias(_) => true,
+            Type::Tuple(head, tail) => head.is_known() && tail.iter().all(Self::is_known),
+            Type::Array(ty, _) => ty.is_known(),
+            Type::Pointer(ty) => ty.is_known(),
+            Type::Ref(ty) => ty.is_known(),
+            Type::Func(args, ty) => args.iter().all(Self::is_known) || ty.is_known(),
         }
     }
 }
